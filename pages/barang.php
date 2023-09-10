59e5ec9642fdd0088592848206a03b15067d00ea
mila amelia <mila.amelia@vneu.co.id>
2023-09-10 13:27:20 +0700

    ok

--- pages/barang.php
+++ pages/barang.php
@@ -56,11 +56,6 @@ include "header.php";
 
 	// bilangan yang diambil ini ditambah 1 untuk menentukan nomor urut berikutnya
 	$urutan++;
-
-	// membentuk kode barang baru
-	// perintah sprintf("%03s", $urutan); berguna untuk membuat string menjadi 3 karakter
-	// misalnya perintah sprintf("%03s", 15); maka akan menghasilkan '015'
-	// angka yang diambil tadi digabungkan dengan kode huruf yang kita inginkan, misalnya BRG 
 	$huruf = "BGR";
 	$kode_otomatis = $huruf . sprintf("%03s", $urutan);
 	?>
